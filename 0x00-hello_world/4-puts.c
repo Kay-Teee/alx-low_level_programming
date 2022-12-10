04922cee6e863f525ba135f5a2db13779c0e6ad9
Emmanuel Taylor <emmanueltaylor954@gmail.com>
2022-12-10 13:57:30 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 
-
 /**
  *
  *  * main - entry point
@@ -12,14 +11,9 @@
  *
  *    */
 
-
-
 int main(void)
 
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
+{		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 
 }
